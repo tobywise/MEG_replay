7acfdd3452ce91eeee4293655fe767d444ddc8d0
hschmi01 <helen.schmidt@tufts.edu>
2018-07-31 15:31:17 -0400

    erp_analysis script helen computer version

--- Analysis/MEG/erp_analysis.py
+++ Analysis/MEG/erp_analysis.py
@@ -10,9 +10,17 @@ import os
 PREPROCESSING
 """
 
+subject_dir = r'/Users/dancingdiva12/Desktop/MEG Data'
+behavioural_dir = r'/Users/dancingdiva12/Desktop/Behavioral Data'
+subject_id = '5379_M009'
+behavioural_id = 'M009'
+
+# Get behavioural 
+behaviour_file = [i for i in os.listdir(os.path.join(behavioural_dir, behavioural_id)) if 'csv' in i][0]
+
 # This code will find any .ds directories in a folder and put them into a list
 # Make sure you have one folder per subject containing all their .ds files
-data_dir = '/path/to/meg/data'
+data_dir = os.path.join(subject_dir, subject_id)
 data = os.listdir(data_dir)
 data = sorted([i for i in data if '.ds' in i])
 
@@ -25,7 +33,7 @@ raw = mne.concatenate_raws(raws)  # Raw is the full task raw data
 del raws  # delete the individual raw data files to save memory
 
 # Find events based on triggers
-events = mne.find_events(raw, stim_channel='UPPT001', shortest_event=1)
+events = mne.find_events(raw, stim_channel='UPPT001', shortest_event=0)
 events[:, 0] += 20  # adjust for presentation latency
 
 # Remove the CTF compensation
@@ -56,7 +64,7 @@ raw.plot_psd(area_mode='range', tmax=10.0,  average=False)
 reject = dict(mag=5e-9)
 
 # Split up into epochs - we're interested in events 40, 8, and 50
-epochs = mne.Epochs(raw, events, tmin=-0.1, tmax=3.01, preload=True,
+epochs = mne.Epochs(raw, events, tmin=-0.1, tmax=4.01, preload=True,
                               event_id=[40, 8, 50],
                               reject=reject)
 
@@ -74,10 +82,10 @@ ica.plot_components()
 ica.plot_sources(epochs, range(0, ica.n_components_))
 
 # And various other things like the power spectrum - the list given to the picks argument here should contain the components you want to look at
-ica.plot_properties(epochs, picks=[0, 3])
+ica.plot_properties(epochs, picks=[0])
 
 # Remove components
-ica.exclude = [0, 3]  # ICA components to remove
+ica.exclude = [0]  # ICA components to remove
 ica.apply(epochs)  # Remove these components
 
 # Save the data (MNE likes epoch files to end in -epo)
@@ -92,29 +100,47 @@ ERP analysis
 epochs = mne.read_epochs(os.path.join(data_dir, 'epoched_data-epo.fif.gz'))
 
 import pandas as pd
-behaviour = pd.read_csv(r'path/to/behavioural/data')
-tt = behaviour['trial_type'].values
-
+behaviour = pd.read_csv(os.path.join(behavioural_dir, behavioural_id, behaviour_file))
+tt = behaviour['trial_type'].values # trial type
+#
+# if len(epochs['40']) + len(epochs['8']) < len(behaviour):
+#     raise ValueError("SUBJECT IS MISSING EVENTS - LEAVE THIS SUBJECT UNTIL TOBY FIXES THINGS")
 
 # Remove extra events - hopefully this will work...
-count = 0
+if len(epochs['40']) + len(epochs['8']) > len(behaviour):
 
-for i in range(len(epochs)):
+    count = 0
 
-    if i < len(epochs):
+    for i in range(len(epochs)):
 
-        if epochs.events[i, 2] == 8:
-            if tt[count] != 0:
-                print tt[count]
-                print i
-                epochs.drop(i)
-                count += 1
-            else:
+        if i < len(epochs):
+
+            if epochs.events[i, 2] == 8:
+                if tt[count] != 0:
+                    print tt[count]
+                    print i
+                    epochs.drop(i)
+                    count += 1
+                else:
+                    # print tt[count]
+                    count += 1
+
+            elif epochs.events[i, 2] == 40:
                 # print tt[count]
                 count += 1
 
-        elif epochs.events[i, 2] == 40:
-            count += 1
+else:
+
+    drops = []
+
+    temp_epochs = epochs[['40', '8']].events
+
+    for n, trial in enumerate(tt):
+        n -= len(drops)
+        if (temp_epochs[n, 2] == 8 and trial == 1) or (temp_epochs[n, 2] == 40 and trial == 0):
+            print "Dropping {0}".format(n)
+            drops.append(n)
+            behaviour = behaviour.drop(n)
 
 
 # Get outcome events, 8 = normal outcomes (when the final state is displayed), 40 = outcome only warnings (we'll
@@ -125,8 +151,14 @@ shock_outcomes = epochs['50']
 
 # Crop the epochs so that they start 0.1 seconds before onset and end after 1 second
 normal_outcomes = normal_outcomes.crop(-0.1, 1)
+outcome_only_shock_outcomes = outcome_only_outcomes.copy().crop(2.9, 4)
 outcome_only_outcomes = outcome_only_outcomes.crop(1.9, 3)
 shock_outcomes = shock_outcomes.crop(-0.1, 0.8)
+
+#normal_outcomes = normal_outcomes.crop(-0.1, 1)
+#outcome_only_outcomes = outcome_only_outcomes.crop(1.9, 3)
+#outcome_only_shock_outcomes = outcome_only_outcomes.crop(2.9, 4)
+#shock_outcomes = shock_outcomes.crop(-0.1, 0.8)
 
 # Adjust the times for the outcome only outcome
 outcome_only_outcomes.times = outcome_only_outcomes.times - 2
@@ -145,15 +177,18 @@ Look at some ERFs!
 # Get the evoked response for shock outcomes
 shock_evoked = shock_outcomes.average()
 shock_evoked.plot_joint(ts_args=dict(time_unit='s'), topomap_args=dict(time_unit='s'))
+plt.savefig('/Users/dancingdiva12/Desktop/UCL/Research Project/thesis/figures/{0}_shocks.png'.format(subject_id))
 
 # If you want to look at power
-from mne.time_frequency import tfr_morlet
-freqs = np.logspace(*np.log10([6, 35]), num=8)
-n_cycles = freqs / 2.  # different number of cycle per frequency
 
-power, itc = tfr_morlet(shock_outcomes, freqs=freqs, n_cycles=n_cycles, use_fft=True,
-                        return_itc=True, decim=3, n_jobs=1)
-power.plot_topo(baseline=(-0.5, 0), mode='logratio', title='Average power')
+
+# from mne.time_frequency import tfr_morlet
+# freqs = np.logspace(*np.log10([6, 35]), num=8)
+# n_cycles = freqs / 2.  # different number of cycle per frequency
+#
+# power, itc = tfr_morlet(shock_outcomes, freqs=freqs, n_cycles=n_cycles, use_fft=True,
+#                         return_itc=True, decim=3, n_jobs=1)
+# power.plot_topo(baseline=(-0.5, 0), mode='logratio', title='Average power')
 
 
 ########################################################
@@ -161,20 +196,37 @@ power.plot_topo(baseline=(-0.5, 0), mode='logratio', title='Average power')
 # And the same for reward outcomes
 reward_evoked = outcomes.average()
 reward_evoked.plot_joint(ts_args=dict(time_unit='s'), topomap_args=dict(time_unit='s'))
+plt.savefig('/Users/dancingdiva12/Desktop/UCL/Research Project/thesis/figures/{0}_rewards.png'.format(subject_id))
 
-# Load behavioural data and recode things
-behavioural = pd.read_csv('path/to/behavioural/data')[1:]
+
+# Load behaviour data and recode things
+behavioural = pd.read_csv(r'/Users/dancingdiva12/Desktop/Behavioral Data')
+behavioural.reset_index()
+trial_info = pd.read_csv('task/Task_information/trial_info.csv')
+behavioural = pd.merge(behavioural, trial_info, on='trial_number')
+
+behavioural['State_3'][behavioural['Reward_received'].isnull()] = behavioural.end_state
+sel = behavioural['Reward_received'].isnull()
+
 behavioural['reward'] = np.nan
 behavioural['reward'][behavioural['State_3'] == 7] = behavioural['State_1_reward'][behavioural['State_3'] == 7]
 behavioural['reward'][behavioural['State_3'] == 8] = behavioural['State_2_reward'][behavioural['State_3'] == 8]
 behavioural['reward'][behavioural['State_3'] == 9] = behavioural['State_3_reward'][behavioural['State_3'] == 9]
 behavioural['reward'][behavioural['State_3'] == 10] = behavioural['State_4_reward'][behavioural['State_3'] == 10]
+behavioural['reward'][~sel] = behavioural['Reward_received']
+
+behavioural['shock'] = np.nan
+behavioural['shock'][behavioural['State_3'] == 7] = behavioural['0_shock'][behavioural['State_3'] == 7]
+behavioural['shock'][behavioural['State_3'] == 8] = behavioural['1_shock'][behavioural['State_3'] == 8]
+behavioural['shock'][behavioural['State_3'] == 9] = behavioural['2_shock'][behavioural['State_3'] == 9]
+behavioural['shock'][behavioural['State_3'] == 10] = behavioural['3_shock'][behavioural['State_3'] == 10]
+behavioural['shock'][~sel] = behavioural['Shock_received']
 
 # Drop the first trial
-outcomes.drop(0)
+behavioural = behavioural[1:]
 
 # Assign behavioural data to metadata attribute
-outcomes.metadata = behavioural.assign(Intercept=1)
+outcomes.metadata = behaviour.assign(Intercept=1)[1:]
 
 # Linear regression
 # The regression has two terms - the intercept (not interesting) and the reward level (interesting)
@@ -182,20 +234,93 @@ outcomes.metadata = behavioural.assign(Intercept=1)
 from mne.stats import linear_regression, fdr_correction
 names = ["Intercept", 'reward']
 res = linear_regression(outcomes, outcomes.metadata[names], names=names)
-
 # Create an "evoked" object for the reward predictor - this represents the beta value for reward level in the model
 # across time points and gradiometers
 reward_evoked = res["reward"].beta
 reward_evoked.plot_joint(ts_args=dict(time_unit='s'), topomap_args=dict(time_unit='s'))
+plt.savefig('/Users/dancingdiva12/Desktop/UCL/Research Project/thesis/figures/{0}_evoked_rewards.png'.format(subject_id))
+
 
 # You can choose which time points the plot shows scalp plots for using the times argument
 reward_evoked.plot_joint(ts_args=dict(time_unit='s'), topomap_args=dict(time_unit='s'), times=[.09, .45])
 
-
-
+shock_evoked.save(os.path.join(data_dir, 'shock_evoked-ave.fif.gz'))
+reward_evoked.save(os.path.join(data_dir, 'reward_evoked-ave.fif.gz'))
 
 """
 Group analysis
 """
 
-# TBC...
+# SHOCK
+
+evokeds = []
+
+for root, dir, files in os.walk(subject_dir):
+    for f in files:
+        if 'ave' in f and 'shock' in f:
+            evokeds.append(os.path.join(root, f))
+
+evokeds = [mne.read_evokeds(i)[0] for i in evokeds]
+#evokeds = mne.combine_evoked(evokeds, 'equal')
+
+grand_average = mne.combine_evoked(evokeds, 'equal')
+
+grand_average.plot_joint(ts_args=dict(time_unit='s'), topomap_args=dict(time_unit='s'))
+
+subject_data = [i.data for i in evokeds]
+subject_data = np.dstack(subject_data)
+
+from scipy.stats import ttest_1samp
+ps = ttest_1samp(subject_data, 0, axis=2)[1]
+
+reject_H0, fdr_pvals = fdr_correction(ps)
+
+grand_average.plot_image(mask=reject_H0, time_unit='s')
+
+# REWARD
+
+evokeds = []
+
+for root, dir, files in os.walk(subject_dir):
+    for f in files:
+        if 'ave' in f and 'reward' in f:
+            evokeds.append(os.path.join(root, f))
+
+evokeds = [mne.read_evokeds(i)[0] for i in evokeds]
+
+grand_average = mne.combine_evoked(evokeds, 'equal')
+
+grand_average.plot_joint(ts_args=dict(time_unit='s'), topomap_args=dict(time_unit='s'))
+
+subject_data = [i.data for i in evokeds]
+subject_data = np.dstack(subject_data)
+
+from scipy.stats import ttest_1samp
+ps = ttest_1samp(subject_data, 0, axis=2)[1]
+
+reject_H0, fdr_pvals = fdr_correction(ps)
+
+grand_average.plot_image(mask=reject_H0, time_unit='s')
+
+
+ev = events[:, 2]
+
+ev = ev[(ev == 30) | (ev == 50)]
+
+
+shock_list = []
+
+for i in range(1, len(ev)):
+    if ev[i] == 30 and ev[i-1] != 50:
+        shock_list.append(0)
+    elif ev[i] == 30 and ev[i-1] == 50:
+        shock_list.append(1)
+
+
+plt.scatter(range(100), behaviour.shock)
+plt.scatter(range(len(shock_list)), shock_list)
+
+
+
+
+
